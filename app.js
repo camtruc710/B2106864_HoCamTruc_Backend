a0440869db9d9810f7057ab6332970d0c0820ca8
Cam Truc <truc.0965000900@gmail.com>
2025-02-09 17:57:45 +0700

    Cai dat express chay thong bao xin chao

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({message: "Welcome to contact book application."});
+    res.json({ message: "Welcome to contact book application."});
 });
 
 module.exports =app;
