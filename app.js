dc1d8b7da39050739f15c734bdfa239bde7a62dd
Cam Truc <truc.0965000900@gmail.com>
2025-02-09 21:58:42 +0700

    Cai dat va xu ly loi

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); // Khai báo thư viện cors (xử lý CORS - Cross-Origin Resource Sharing).
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter); // Sử dụng contactsRouter cho các route /api/contacts
 
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application."});
+// Middleware xử lý lỗi 404
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resoure not found")); // Tạo lỗi 404 và chuyển cho middleware xử lý lỗi
 });
 
-app.use("/api/contacts", contactsRouter);
+// Middleware xử lý lỗi chung
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({ // Trả về mã lỗi hoặc 500 nếu không có
+        message: err.message || "Internal Server Error", // Trả về message lỗi hoặc "Internal Server Error"
+    });
+});
 
+// Route mặc định (/)
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application."}); // Trả về message chào mừng
+});
 module.exports = app;
